413ba43a5708e7c9d1be5546e7419daf57b5157a
menghot <menghot@gmail.com>
2023-11-26 11:53:15 +0800

    update

--- demo_trigger_dag_run.py
+++ demo_trigger_dag_run.py
@@ -19,6 +19,21 @@ with models.DAG(
         conf={"notice": "Hello DAG!"}
     )
 
+    # spark-submit --master k8s://https://10.194.183.222:6443 --deploy-mode cluster --name spark-pi --class
+    # org.apache.spark.examples.SparkPi --conf spark.executor.instances=2 --conf
+    # spark.kubernetes.node.selector.kubernetes.io/hostname=node-10-194-183-226 --conf
+    # spark.kubernetes.container.image=apache/spark:3.5.0 --conf spark.kubernetes.submission.waitAppCompletion=true
+    # --conf spark.kubernetes.driver.request.cores=1 --conf spark.kubernetes.driver.limit.cores=1 --conf
+    # spark.driver.memory=2g --conf spark.executor.memory=2g --conf spark.kubernetes.executor.request.cores=1 --conf
+    # spark.kubernetes.executor.limit.cores=1 --conf spark.kubernetes.authenticate.driver.serviceAccountName=spark
+    # --conf spark.kubernetes.driver.volumes.persistentVolumeClaim.vol1.options.claimName=spark-data --conf
+    # spark.kubernetes.driver.volumes.persistentVolumeClaim.vol1.mount.path=/opt/spark/logs --conf
+    # spark.kubernetes.executor.volumes.persistentVolumeClaim.vol1.options.claimName=spark-data --conf
+    # spark.kubernetes.executor.volumes.persistentVolumeClaim.vol1.mount.path=/opt/spark/logs --conf
+    # spark.kubernetes.namespace=dtp local:///opt/spark/examples/jars/spark-examples_2.12-3.5.0.jar
+
+    # --conf spark.kubernetes.driver.volumes.hostPath.logs_dir.mount.path=/opt/spark/logs
+
     submit_spark_job = SparkBashSubmitOperator(
         task_id="submit_spark_job",
         bash_command="spark-submit "
@@ -37,11 +52,9 @@ with models.DAG(
                      "--conf spark.kubernetes.executor.request.cores=1 "
                      "--conf spark.kubernetes.executor.limit.cores=1 "
                      "--conf spark.kubernetes.authenticate.driver.serviceAccountName=spark "
-                     "--conf spark.kubernetes.driver.volumes.persistentVolumeClaim.vol1.options.claimName=spark-data "
-                     "--conf spark.kubernetes.driver.volumes.persistentVolumeClaim.vol1.mount.path=/opt/spark/logs "
-                     "--conf spark.kubernetes.executor.volumes.persistentVolumeClaim.vol1.options.claimName=spark-data "
-                     "--conf spark.kubernetes.executor.volumes.persistentVolumeClaim.vol1.mount.path=/opt/spark/logs "
                      "--conf spark.kubernetes.namespace=dtp "
+                     "--conf spark.kubernetes.driver.volumes.hostPath.logs_dir.mount.path=/opt/spark/logs"
+                     "--conf spark.kubernetes.executor.volumes.hostPath.logs_dir.mount.path=/opt/spark/logs"
                      "local:///opt/spark/examples/jars/spark-examples_2.12-3.5.0.jar",
     )
 
